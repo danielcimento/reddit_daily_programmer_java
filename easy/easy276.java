5eaf049573eda3dee80f451f3ca1e0299d9b742a
Daniel Cimento <daniel.cimento@mail.mcgill.ca>
2016-07-24 00:37:10 -0700

    Refactored Easy Challenge 276

--- easy/easy276.java
+++ easy/easy276.java
@@ -10,62 +10,64 @@ public class easy276{
 	public static void recktangles(String word, int width, int height){
 		int rows = word.length() + (height-1) * (word.length()-1);
 		int columns = word.length() + (width-1) * (word.length()-1);
+		//"Fencepost" problem. Start with length, then add length-1 for every next section added
 		char[][] grid = new char[rows][columns]; //rows, columns
 		boolean reversing = false;
-		boolean rowReversing = false;
-		boolean columnReversing = false;		
-		int horizontalPointer = 0;
+		boolean lineReversing = false;		
+		int pointer = 0;
 
 		//fill out rows
 		for(int i = 0; i < rows; i += (word.length() - 1)){
-			if(rowReversing){
-				horizontalPointer = word.length() - 1;
+			if(lineReversing){
+				pointer = word.length() - 1;
 				reversing = true;
 			}else{
-				horizontalPointer = 0;
+				pointer = 0;
 				reversing = false;
 			}
 			for(int j = 0; j < columns; j++){
-				grid[i][j] = word.charAt(horizontalPointer);
-				if((horizontalPointer == word.length() - 1 && !reversing) || (horizontalPointer == 0 && reversing)){
+				grid[i][j] = word.charAt(pointer);
+				//end/beginning of word reached
+				if((pointer == word.length() - 1 && !reversing) || (pointer == 0 && reversing)){
 					reversing = !reversing;
 				}
 				if(!reversing){
-					horizontalPointer++;
+					pointer++;
 				}else{
-					horizontalPointer--;
+					pointer--;
 				}
 			}
-			rowReversing = !rowReversing;
+			lineReversing = !lineReversing;
 		}
-		
+		lineReversing = false;	
 		//fill out columns
 		for(int i = 0; i < columns; i += (word.length() - 1)){
-			if(columnReversing){
-				horizontalPointer = word.length() - 1;
+			if(lineReversing){
+				pointer = word.length() - 1;
 				reversing = true;
 			}else{
-				horizontalPointer = 0;
+				pointer = 0;
 				reversing = false;
 			}
 			for(int j = 0; j < rows; j++){
-				grid[j][i] = word.charAt(horizontalPointer);
-				if((horizontalPointer == word.length() - 1 && !reversing) | (horizontalPointer == 0 && reversing)){
+				grid[j][i] = word.charAt(pointer);
+				if((pointer == word.length() - 1 && !reversing) | (pointer == 0 && reversing)){
 					reversing = !reversing;
 				}
 
 				if(!reversing){
-					horizontalPointer++;
+					pointer++;
 				}else{
-					horizontalPointer--;
+					pointer--;
 				}
 			}
-			columnReversing = !columnReversing;
+			lineReversing = !lineReversing;
 		}
 
 		//print
 		for(int i = 0; i < rows; i++){
 			for(int j = 0; j < columns; j++){
+				//didn't get filled in, default to 2 spaces
 				if(grid[i][j] != '\u0000')
 					System.out.print(grid[i][j] + " ");
 				else
